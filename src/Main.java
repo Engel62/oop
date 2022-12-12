3e43bd3c540c326e0e33eb6ea211a0928ee7de3c
Engel62 <sr.engel@bk.ru>
2022-12-12 17:47:10 +0300

    ДЗ 1 задание 1-2 выпонены

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-       Human maxim = new Human(1988, "Максим", "Минск");
-        maxim.jobTitle = "бренд-менеджер";
-       Human anya = new Human(1993, "Аня", "Москва" );
-        anya.jobTitle = "методист образовательных программ";
-       Human katy = new Human(1992, "Катя", "Калиниград" );
-        katy.jobTitle = "продакт-менеджер";
-       Human artem = new Human(1995, "Артем", "Москвва" );
-        artem.jobTitle = "директор по развитию бизнеса";
+       Human maxim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
+       Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+       Human katy = new Human(1992, "Катя", "Калиниград","продакт-менеджер");
+       Human artem = new Human(1995, "Артем", "Москвва", "директор по развитию бизнеса");
+
 
        maxim.informationAboutPerson();
        anya.informationAboutPerson();
