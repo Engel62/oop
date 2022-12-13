ce352190b30b30b7f30e031717d5dca3cd490dad
Engel62 <sr.engel@bk.ru>
2022-12-13 12:15:23 +0300

    задание 3

--- src/Human.java
+++ src/Human.java
@@ -7,10 +7,25 @@ public class Human {
 
 
     public Human(int yearOfBirth, String name, String town, String jobTitle) {
+        if (yearOfBirth < 0) {
+            yearOfBirth = 0;
+        }
         this.yearOfBirth = yearOfBirth;
+        if (name == null || name.isEmpty()) {
+            name = "информация не указанна";
+        }
         this.name = name;
+
+        if (town == null || town.isEmpty()) {
+            town = "информация не указанна";
+        }
         this.town = town;
+
+        if (jobTitle == null || jobTitle.isEmpty()) {
+            jobTitle = "информация не указанна";
+        }
         this.jobTitle = jobTitle;
+
     }
 
     void informationAboutPerson() {
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
        Human maxim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
        Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
-       Human katy = new Human(1992, "Катя", "Калиниград","продакт-менеджер");
+       Human katy = new Human(1992, "Катя", "Калиниград",null);
        Human artem = new Human(1995, "Артем", "Москвва", "директор по развитию бизнеса");
 
 
