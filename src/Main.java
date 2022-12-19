a032cf0bcdd358af1d031acfc2c1589fd788a2e9
Engel62 <sr.engel@bk.ru>
2022-12-19 19:56:52 +0300

    Теория

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
+import animal.Cat;
+import animal.Cat.Breed;
+
 public class Main {
+
     public static void main(String[] args) {
+        Breed brShHair = new Breed("British Shorthair", "GB", "short");
 
-       Human maxim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
-       Human anya = new Human(1993, "Аня", "Москва", "методист образовательных программ");
-       Human katy = new Human(1992, "Катя", "Калиниград","продакт-менеджер");
-       Human artem = new Human(1995, "Артем", "Москвва", "директор по развитию бизнеса");
+        Cat murzik = new Cat("Мурзик", 1);
+        murzik.setBreed(brShHair);
 
+        Cat.FavToy murzikToy = murzik.new FavToy("Рыбка", "заводная");
+        murzik.setToy(murzikToy);
 
-       maxim.informationAboutPerson();
-       anya.informationAboutPerson();
-       katy.informationAboutPerson();
-       artem.informationAboutPerson();
+        System.out.println("Игрушка " + murzikToy.getName() + ", нравится коту " + murzikToy.getCatName());
 
+        murzik.meow();
 
+        Cat begemot = new Cat("Бегемот", -5);
+        begemot.setName(null);
+        begemot.meow();
+
+        System.out.println("У кота " + murzik.getName() + " " + murzik.getFriends().length + " друзей");
+        murzik.addFriends(begemot);
+        System.out.println("У кота " + murzik.getName() + " " + murzik.getFriends().length + " друзей");
+        murzik.getFriends()[0].meow();
+
+        System.out.println("Создано котов " + Cat.getCount());
     }
 }
